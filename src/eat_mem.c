f4b1ac9a055fa8a6f44cc5a72e4f80e7371afb50
Kurt Lust <Kurt.Lust@uantwerpen.be>
2020-02-21 11:14:48 +0100

    Minor updates to print a bit more information.

--- src/eat_mem.c
+++ src/eat_mem.c
@@ -11,7 +11,7 @@
 const unsigned int MB         = 1024 * 1024;
 const unsigned int default_GB = 3;
 const unsigned int max_GB     = 256;
-const unsigned int sleeptime  = 20000;       // Time to sleep after allocating one block (microseconds)
+const unsigned int sleeptime  = 10000;       // Time to sleep after allocating one block (microseconds)
 
 int main(int argc, char *argv[])
 {
@@ -20,51 +20,54 @@ int main(int argc, char *argv[])
     char* mem[MB];
 
     if (argc > 2) {
-        printf("Usage: eat_mem [number]\n");
-        printf("  Allocate and use [number] Gigabyte of memory.\n");
-        printf("  Default is %s Gigabyte.\n", default_GB);
+        printf( "Usage: eat_mem [number]\n" );
+        printf( "  Allocate and use [number] Gigabyte of memory.\n" );
+        printf( "  Default is %u Gigabyte, maximum accepted value is %u Gigabyte.\n",
+        		default_GB, max_GB );
         exit(1);
     }
 
     // Calculate the amount of memory to allocate
-    // Default = 5Gb
+    // Default = value of default_GB
     if (argc == 2) {
         tot_GB = atoi(argv[1]);
         if (tot_GB <= 0)
             tot_GB = default_GB;
         else if (tot_GB > max_GB) {
-            printf("eat_mem: Exceeded maximum, reset to %d Gigabyte.\n", max_GB);
+            printf( "eat_mem: Exceeded maximum, reset to %d Gigabyte.\n", max_GB );
             tot_GB = max_GB;
         }
     }
-    printf("eat_mem: Consuming %d Gigabyte of memory.\n", tot_GB);
+    printf( "eat_mem: Consuming %d Gigabyte of memory.\n", tot_GB );
 
     // Fill up a buffer of 1 MB with random digits
-    for (int i = 0; i < MB; i++) buffer[i] = rand()%256;
+    for (int i = 0; i < MB; i++) buffer[i] = rand() % 256;
 
     // Consume memory, 1 MB at a time
     for (int i = 0; i < tot_GB*1024; i++) {
-        mem[i] = (char *) malloc(MB*sizeof(char));
+        mem[i] = (char *) malloc( MB*sizeof(char) );
         if (mem[i] == NULL) {
-            printf("eat_mem: ERROR: Out of memory\n");
+            printf( "eat_mem: ERROR: Out of memory\n" );
             return 1;
         }
         else {
-            memcpy(mem[i], (void *) buffer, MB);
-            usleep (sleeptime); // Wait 20 microseconds
+            memcpy( mem[i], (void *) buffer, MB );
+            usleep( sleeptime ); // Wait some time
         }
     }
 
     // Sleep 10 seconds, so that you have time to monitor the full memory allocation
-    sleep (10);
+    sleep( 10 );
 
     // Free memory, 1 MB at a time
+    printf( "eat_mem: Now freeing memory.\n" );
     for (int i = 0; i < tot_GB*1024; i++) {
-        free(mem[i]);
+        free( mem[i] );
+        usleep( sleeptime );
     }
 
     // Sleep 10 seconds, so that you have time to monitor the freed-up memory
-    sleep (10);
+    sleep( 10 );
 
     return 0;
 }
